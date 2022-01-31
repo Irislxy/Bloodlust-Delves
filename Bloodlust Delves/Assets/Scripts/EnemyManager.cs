9fdaa1d8a804ef7391bfb059117bfaebe18ae690
amanushri <98077844+amanushri@users.noreply.github.com>
2022-01-31 13:51:48 +0800

    Enemy Diezzzz

---
+++ Bloodlust Delves/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    public float damage = 10f;
+    public float health = 100f;
+
+    public void Hit(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(enemy);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<PlayerManager>().Hit(damage);
+        }
+    }
+}
